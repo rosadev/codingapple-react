b64f3c6b8e88a8827175bd22d61ff538ebae626b
rosadev <noblerosakim@gmail.com>
2022-08-18 00:07:23 +0900

    add shoeshop layout

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import { useState } from 'react';
 import { Button, Navbar, Container, Nav } from 'react-bootstrap';
+// import 작명 from './data.js';
+// import {a, b} from './data.js'; // 중괄호로 가져올땐 자유로운 작명 안됨
+// import bg from './img/bg.png';
+import data from './data.js';
 
 function App() {
+  let [shoes] = useState(data)
+
   return (
     <div className="App">
      <Navbar bg="dark" variant="dark">
@@ -14,6 +21,29 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+    <div className="main-bg">
+      {/* <img src={process.env.PUBLIC_URL + '/bg.png'} width="100%"/> */}
+    </div>
+    <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
+          </div>
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
+            <h4>{shoes[2].title}</h4>
+            <p>{shoes[2].price}</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
