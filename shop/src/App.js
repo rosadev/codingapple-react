9e1e30490f851249ed9443a56dd549bf27b5165a
rosadev <noblerosakim@gmail.com>
2022-08-19 14:34:19 +0900

    add map function

--- shop/src/App.js
+++ shop/src/App.js
@@ -25,27 +25,31 @@ function App() {
       {/* <img src={process.env.PUBLIC_URL + '/bg.png'} width="100%"/> */}
     </div>
     <div className="container">
-        <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>{shoes[1].title}</h4>
-            <p>{shoes[1].price}</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>{shoes[2].title}</h4>
-            <p>{shoes[2].price}</p>
-          </div>
-        </div>
+      <div className="row">
+        {/* <Card shoes={shoes[0]} i={1}></Card>
+        <Card shoes={shoes[1]} i={2}></Card>
+        <Card shoes={shoes[2]} i={3}></Card>    */}
+        {
+          shoes.map((a, i)=> {
+            return(
+            <Card shoes={shoes[i]} i={i}></Card>
+            )
+          })
+        }
       </div>
+    </div>
 
     </div>
   );
 }
-
 export default App;
+
+function Card(props){
+  return(
+    <div className="col-md-4">
+      <img src={'https://codingapple1.github.io/shop/shoes'+ (props.i+1) +'.jpg'} width="80%" />
+      <h4>{props.shoes.title}</h4>
+      <p>{props.shoes.price}</p>
+    </div>
+  )
+}
