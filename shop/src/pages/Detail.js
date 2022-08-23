49a513073c125ce0448e7723702d9a48a676b69e
rosadev <noblerosakim@gmail.com>
2022-08-23 23:51:50 +0900

    add useEffect func

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -35,10 +35,37 @@ function Detail(props) {
     // 컴포넌트에 갈고리 다는법
     useEffect(()=>{
       // 타이머 주는법
-      setTimeout(()=> {setAlert(false)}, 2000)
+      let a = setTimeout(()=> {setAlert(false)}, 2000)
       // mount, update시 코드 실행
       console.log('안녕')
+
+      // 서버로 데이터 요청하는 코드(2초 소요) : 2초 사이에 재렌더링 되면 계속 요청되어 버그 많아질듯
+      // 기존 데이터 요청을 제거할 필요
+
+      // useEffect 동작 전에 실행 return()=>{} : clean up function
+      // 타이머 제거해주는 함수
+      // mount시 실행안됨. unmount시 실행됨
+      return ()=> {
+        clearTimeout(a)
+      }
     }, []) 
+
+    /* 빡통식 정리법 */
+    // 1. 재렌더링마다 코드 실행하고 싶으면
+    useEffect(()=>{ })
+    // 2. mount시 1회 코드실행하고 싶으면
+    useEffect(()=>{ }, [])
+    // 3. unmount시 1회 코드 실행하고 싶으면
+    useEffect(()=>{ 
+        return()=>{
+
+        }
+    }, [])
+    // 4. useEffect 실행 전에 뭔가 실행하려면 언제나 return()=>{ } 
+    // useEffect 내부에 clean up function을 써준다
+    // 5. 특정 state 변경시에만 실행하려면 [state명] : dependency[]에 변수명 넣어서 
+   
+
 
   
   // 컴포넌트 업데이트 = 재렌더링 
