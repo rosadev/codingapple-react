29bc2ebed65f87dfa38396ba6b0b4d2b018a258f
rosadev <noblerosakim@gmail.com>
2022-09-01 22:45:52 +0900

    update

--- shop/src/pages/Detail.js
+++ shop/src/pages/Detail.js
@@ -45,9 +45,9 @@ function Detail(props) {
   useEffect(()=>{
     let 꺼낸거 = localStorage.getItem('watched')
     꺼낸거 = JSON.parse(꺼낸거)
-    console.log(꺼낸거)
     꺼낸거.push(찾은상품.id)
-    console.log(꺼낸거)
+    꺼낸거 = new Set(꺼낸거) // 중복제거
+    꺼낸거 = Array.from(꺼낸거) // 다시 어레이로
     localStorage.setItem('watched', JSON.stringify(꺼낸거))
   }, [])
   
